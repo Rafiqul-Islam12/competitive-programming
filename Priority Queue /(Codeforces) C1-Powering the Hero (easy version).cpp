47bc52faf9bfe2a6ba0b05fc7989ef6cd16b238a
Rafiqul-Islam12 <rafiq.cse.cou@gmail.com>
2025-03-13 00:54:00 +0600

    Update code

--- Priority Queue /(Codeforces) C1-Powering the Hero (easy version).cpp
+++ Priority Queue /(Codeforces) C1-Powering the Hero (easy version).cpp
@@ -1,1 +1,45 @@
-
+//Problem link:- https://codeforces.com/contest/1800/problem/C1
+#include<bits/stdc++.h>
+#define ll long long int
+#define MOD 1000000007
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        ll n,k,p,q,r,x,y,z,i,j=0,sum=0,ans=0,cnt=0;
+        cin >> n;
+ 
+        string s;
+        vector<ll>v(n);
+        map<ll,ll>m;
+        set<ll>st;
+ 
+        for(i=0; i<n; i++)
+        {
+            cin >> v[i];
+        }
+ 
+        priority_queue<ll>pq;
+ 
+        for(i=0; i<n; i++)
+        {
+            if(v[i]==0)
+            {
+                if(pq.empty())
+                    continue;
+                else
+                {
+                    sum += pq.top();
+                    pq.pop();
+                }
+            }
+            else
+                pq.push(v[i]);
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
