dd96337838d2a5319335b2b37ee1cfd1b25cc793
Rafiqul-Islam12 <rafiq.cse.cou@gmail.com>
2025-05-18 23:03:18 +0600

    Update Template.cpp

--- Ordered Set/Template.cpp
+++ Ordered Set/Template.cpp
@@ -5,17 +5,16 @@ using namespace std;
 
 // Header files, namespaces,
 // macros as defined above
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
+template <typename T> using ordered_set = tree<T, null_type, greater_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
 
 // Driver program to test above functions
 int main()
 {
     // Ordered set declared with name o_set
-    ordered_set o_set;
+    ordered_set<int> o_set;
 
     // insert function to insert in
     // ordered set same as SET STL
