5df338cbfeadb869b77b1e8966d75c05431d3567
Rafiqul-Islam12 <rafiq.cse.cou@gmail.com>
2025-05-20 20:56:12 +0600

    Update Binary Exponentiation.cpp

--- Number Theory/Binary Exponentiation.cpp
+++ Number Theory/Binary Exponentiation.cpp
@@ -1,1 +1,14 @@
-
+/*
+Description: Calculate the values 𝑎^𝑏 modulo 109+7. 
+Complexity: O(log(b)) 
+*/
+const ll MOD = 1000000007; 
+ll BinExpo (ll a, ll b) { 
+    ll ans = 1; 
+    while (b) { 
+        if (b & 1) ans = (ans * a) % MOD; 
+        a = (a * a) % MOD; 
+        b >>= 1; 
+    } 
+    return ans; 
+} 
