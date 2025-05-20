7d92d43efefe17980b43d4c97429c7c1427eef48
Rafiqul-Islam12 <rafiq.cse.cou@gmail.com>
2025-05-20 21:06:19 +0600

    Update Kadane’s Algorithm.cpp

--- Number Theory/Kadane’s Algorithm.cpp
+++ Number Theory/Kadane’s Algorithm.cpp
@@ -1,1 +1,16 @@
-
+/* 
+Description: Find the maximum subarray sum. 
+Complexity: O(N) 
+*/
+int KadanesAlgo(int a[ ], int N) { 
+    int MaxSum = INT_MIN; 
+    int CurrentSum = 0; 
+ 
+    for (int i=0; i<N; i++) { 
+        CurrentSum += a[i]; 
+        MaxSum = max (MaxSum, CurrentSum); 
+        if (CurrentSum < 0) 
+            CurrentSum = 0; 
+    } 
+    return MaxSum; 
+}
