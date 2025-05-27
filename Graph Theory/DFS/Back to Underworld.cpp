386145dd53296d218f110022ab292a79997a858f
Rafiqul-Islam12 <rafiq.cse.cou@gmail.com>
2025-05-28 01:58:01 +0600

    (LightOJ)

---
+++ Graph Theory/DFS/Back to Underworld.cpp
@@ -0,0 +1,106 @@
+//Problem link: https://lightoj.com/problem/back-to-underworld
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef set<ll> sl;
+typedef map<ll,ll> ml;
+/*
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type,
+less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+*/
+#define pb push_back
+#define F first
+#define S second
+#define mp make_pair
+#define endl '\n'
+#define all(a) (a).begin(),(a).end()
+#define allr(a) (a).rbegin(),(a).rend()
+#define srt(v) sort(v.begin(), v.end())
+#define rsrt(v) sort(v.rbegin(), v.rend())
+#define sz(x) (int)x.size()
+#define mx_int_prime 999999937
+const int N = 1e5+10;
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+#define lcm(a,b) (a*b)/(__gcd(a,b))
+
+#define Rafiq ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+const int mx = 1e5+123;
+vector<int>adj[mx];
+bool vis[mx];
+bool isNode[mx];
+int cnt, cnt1;
+
+void dfs(int u, int col) {
+    vis[u] = 1;
+    if (col==1) cnt1++;
+    cnt++;
+
+    int tmp;
+    if(col==1) tmp = 2;
+    else tmp = 1;
+
+    for(auto v:adj[u]) {
+        if(vis[v]==0)
+            dfs(v, tmp);
+    }
+}
+
+int main() {
+    Rafiq;
+    int t;
+    cin >> t;
+    int n = t;
+
+    while(t--) {
+        for(int i=0; i<mx; i++) {
+            adj[i].clear();
+            isNode[i] = 0;
+            vis[i] = 0;
+        }
+
+        int m;
+        cin >> m;
+        for(int i=1; i<=m; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].pb(v);
+            adj[v].pb(u);
+
+            isNode[u] = 1;
+            isNode[v] = 1;
+        }
+
+        int ans = 0;
+        for(int i=1; i<=2e4; i++) {
+            if(isNode[i]==1 && vis[i]==0) {
+                cnt=0, cnt1=0;
+                dfs(i,1);
+                ans += max(cnt1,cnt-cnt1);
+            }
+        }
+        cout << "Case " << n-t << ": " << ans << endl;
+    }
+    return 0;
+}
