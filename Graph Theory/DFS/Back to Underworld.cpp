7b6b225b0f7f849f38f792f6abc19992db918dc4
Rafiqul-Islam12 <rafiq.cse.cou@gmail.com>
2025-06-02 14:26:59 +0600

    (LightOJ)

--- Graph Theory/DFS/Back to Underworld.cpp
+++ Graph Theory/DFS/Back to Underworld.cpp
@@ -1,50 +1,7 @@
 //Problem link: https://lightoj.com/problem/back-to-underworld
 #include<bits/stdc++.h>
 using namespace std;
-
-typedef long long ll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef pair<int,int> pii;
-typedef pair<ll, ll> pll;
-typedef vector<pii> vii;
-typedef vector<pll> vll;
-typedef set<ll> sl;
-typedef map<ll,ll> ml;
-/*
-#include<ext/pb_ds/assoc_container.hpp>
-#include<ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds;
-template <typename T> using ordered_set = tree<T, null_type,
-less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
-*/
-#define pb push_back
-#define F first
-#define S second
-#define mp make_pair
-#define endl '\n'
-#define all(a) (a).begin(),(a).end()
-#define allr(a) (a).rbegin(),(a).rend()
-#define srt(v) sort(v.begin(), v.end())
-#define rsrt(v) sort(v.rbegin(), v.rend())
-#define sz(x) (int)x.size()
-#define mx_int_prime 999999937
-const int N = 1e5+10;
-const double PI = acos(-1);
-const double eps = 1e-9;
-const int inf = 2000000000;
-const ll infLL = 9000000000000000000;
-#define MOD 1000000007
-
-#define mem(a,b) memset(a, b, sizeof(a) )
-#define gcd(a,b) __gcd(a,b)
-#define lcm(a,b) (a*b)/(__gcd(a,b))
-
 #define Rafiq ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-#define fraction(a) cout.unsetf(ios::floatfield); cout.precision(a); cout.setf(ios::fixed,ios::floatfield);
-#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
 
 const int mx = 1e5+123;
 vector<int>adj[mx];
@@ -72,7 +29,6 @@ int main() {
     int t;
     cin >> t;
     int n = t;
-
     while(t--) {
         for(int i=0; i<mx; i++) {
             adj[i].clear();
